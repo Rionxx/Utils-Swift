cf39ea090410b9d3e53ef0a10e610753fe471b26
Rion <rionfx.42@gmail.com>
2021-10-10 09:51:04 +0900

    Created Project

---
+++ UIScreen.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by FX on 2021/10/10.
+//
+
+import SwiftUI
+
+extension UIScreen {
+    static let screenWidth: CGFloat = UIScreen.main.bounds.size.width
+    static let screenHeight: CGFloat = UIScreen.main.bounds.size.height
+    static let screenSize: CGFloat = UIScreen.main.bounds.size
+}
